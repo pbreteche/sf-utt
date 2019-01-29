146cdacc326ad63fb85013afee272ba03b369c37
Pierre Bretéché <pbreteche@dawan.fr>
2019-01-29 11:47:04 +0100

    première traduction

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,9 +7,22 @@ use AppBundle\Services\VisitInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/i18n")
+     */
+    public function i18nAction(TranslatorInterface $translator)
+    {
+        $message = $translator->trans('app.training.controller_message');
+
+        return $this->render('default/i18n.html.twig', [
+            'controllerMessage' => $message,
+        ]);
+    }
+
     /**
      * @Route("/{url}", name="homepage")
      */
